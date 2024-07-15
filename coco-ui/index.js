5d13a1164a24e1e9f7252822bb8c1159df04ac11
Belén Albeza <belen-albeza@users.noreply.github.com>
2024-07-15 18:09:43 +0200

    Scaffold COCO vm and UI app (#2)
    * Create coco-vm crate
    
    * Init coco-ui web app
    
    * Load ROM from web-land and run it in Rust

---
+++ coco-ui/index.js
@@ -0,0 +1,47 @@
+import initWasm, { runRom } from "./vendor/coco_vm.js";
+
+async function handleFile(file) {
+  const buffer = file.arrayBuffer();
+  const rom = new Uint8Array(buffer);
+
+  const output = runRom(rom);
+  console.debug(`PC: 0x${output.pc.toString(16)}`);
+}
+
+async function fetchRom(path) {
+  try {
+    const response = await fetch(path);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
+}
+
+function setupRomSelector(selectEl, defaultRom) {
+  const defaultOption = selectEl.querySelector(`option[value="${defaultRom}"]`);
+  defaultOption.selected = true;
+
+  selectEl.addEventListener("change", async (event) => {
+    const romUrl = `/roms/${event.target.value}`;
+    const rom = await fetchRom(romUrl);
+    if (rom) {
+      await handleFile(rom);
+    }
+  });
+}
+
+async function main() {
+  const _ = await initWasm("./vendor/coco_vm_bg.wasm");
+  const romSelector = document.querySelector("#coco-rom-selector");
+
+  const defaultRom = "empty.rom";
+  await setupRomSelector(romSelector, defaultRom);
+
+  const rom = await fetchRom(`/roms/${defaultRom}`);
+  if (rom) {
+    await handleFile(rom);
+  }
+}
+
+main();
