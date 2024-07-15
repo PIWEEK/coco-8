2dce50595963306c845167bb65c6438a3d252b4b
Belén Albeza <belen@hey.com>
2024-07-15 12:15:06 +0200

    Add cpu scaffolding

--- coco-core/src/lib.rs
+++ coco-core/src/lib.rs
@@ -1,5 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+#![forbid(unsafe_code)]
+
+#[derive(Debug)]
+struct Stack {
+    data: [u8; 0x100],
+    index: u8,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Self {
+            data: [0_u8; 0x100],
+            index: 0,
+        }
+    }
+}
+
+pub struct Cpu<'a> {
+    /// Main memory (64 Kb)
+    ram: &'a mut [u8; 0x10000],
+    /// Main, working stack (256 bytes)
+    stack: Stack,
+    /// Return stack (256 bytes)
+    ret_stack: Stack,
+    /// Program counter
+    pc: u16,
+}
+
+impl<'a> Cpu<'a> {
+    pub fn new(ram: &'a mut [u8; 0x10000]) -> Self {
+        Self {
+            ram,
+            stack: Stack::new(),
+            ret_stack: Stack::new(),
+            pc: 0,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +43,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn creates_cpu() {
+        let mut memory = [0_u8; 0x10000];
+        let cpu = Cpu::new(&mut memory);
+
+        assert_eq!(cpu.pc, 0);
     }
 }
