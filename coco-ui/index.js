b00827f23c53d250a0d2c7a85cb71569374c8fec
Belén Albeza <belen@hey.com>
2024-07-18 21:10:22 +0200

    Show source for rom

--- coco-ui/index.js
+++ coco-ui/index.js
@@ -15,7 +15,12 @@ async function handleFile(file) {
   }
 }
 
-async function fetchRom(path) {
+async function handleSource(file) {
+  const contents = await file.text();
+  document.querySelector("#coco-source").innerHTML = contents;
+}
+
+async function fetchFile(path) {
   try {
     const response = await fetch(path);
     return response;
@@ -25,40 +30,75 @@ async function fetchRom(path) {
   return null;
 }
 
-function setupRomSelector(selectEl, defaultRom) {
+function setupRomSelector(defaultRom, { selectEl, ...others }) {
   const defaultOption = selectEl.querySelector(`option[value="${defaultRom}"]`);
   defaultOption.selected = true;
 
   selectEl.addEventListener("change", async (event) => {
-    const romUrl = `/roms/${event.target.value}`;
-    const rom = await fetchRom(romUrl);
-    if (rom) {
-      await handleFile(rom);
-    }
+    const filename = event.target.value;
+    await fetchBytecodeAndSource(filename, { ...others });
   });
 }
 
-function setupControls() {
-  let showBytecodeCheckbox = document.querySelector("#coco-show-bytecode");
-  let bytecodeEl = document.querySelector("#coco-bytecode");
-
+function setupControls({
+  showBytecodeCheckbox,
+  showSourceCheckbox,
+  bytecodeEl,
+  sourceEl,
+}) {
   showBytecodeCheckbox.addEventListener("change", (event) => {
     bytecodeEl.style.display = event.target.checked ? "block" : "none";
+  });
+
+  showSourceCheckbox.addEventListener("change", (event) => {
+    sourceEl.style.display = event.target.checked ? "block" : "none";
+  });
+}
+
+async function fetchBytecodeAndSource(
+  filename,
+  { showSourceCheckbox, showBytecodeCheckbox, bytecodeEl, sourceEl }
+) {
+  showSourceCheckbox.disabled = true;
+  showBytecodeCheckbox.disabled = true;
+
+  bytecodeEl.innerHTML = "Loading…";
+  sourceEl.innerHTML = "Loading…";
+
+  fetchFile(`roms/${filename}.rom`).then(async (rom) => {
+    await handleFile(rom);
+    showBytecodeCheckbox.disabled = false;
+  });
+
+  fetchFile(`roms/${filename}.tal`).then(async (source) => {
+    await handleSource(source);
+    showSourceCheckbox.disabled = false;
   });
 }
 
 async function main() {
   const _ = await initWasm("./vendor/coco_ui_bg.wasm");
+
   const romSelector = document.querySelector("#coco-rom-selector");
+  const showBytecodeCheckbox = document.querySelector("#coco-show-bytecode");
+  const bytecodeEl = document.querySelector("#coco-bytecode");
+  let showSourceCheckbox = document.querySelector("#coco-show-source");
+  let sourceEl = document.querySelector("#coco-source");
 
-  const defaultRom = "sprite_move.rom";
-  setupRomSelector(romSelector, defaultRom);
-  setupControls();
+  let debugControls = {
+    sourceEl,
+    showBytecodeCheckbox,
+    bytecodeEl,
+    showSourceCheckbox,
+  };
 
-  const rom = await fetchRom(`roms/${defaultRom}`);
-  if (rom) {
-    await handleFile(rom);
-  }
+  const defaultRom = "sprite";
+  setupRomSelector(`${defaultRom}`, {
+    selectEl: romSelector,
+    ...debugControls,
+  });
+  setupControls(debugControls);
+  await fetchBytecodeAndSource(defaultRom, debugControls);
 }
 
 main();
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 mkdir -p dist/roms
-cp -r coco-ui/roms/*.rom dist/roms
+cp -r coco-ui/roms dist/
 cp -r coco-ui/vendor dist/
 cp coco-ui/index.* dist/
 cp coco-ui/styles.css dist/
